d9ad6bc6615cac75d44a7bf4bb8b3909230040db
Dayton <kirylmitch@gmail.com>
2021-11-01 12:10:30 -0400

    commit tree 8a92832a3c81b24bb7a4e630bf3f1d8fbe5073af

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Straigus Rheyist";
+//const firstInitial = currentUser[0]
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+//const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
